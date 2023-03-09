f887b6ac1b90ec4584ac47f7be8497c3257bbbd3
coffeequickly <jangwonseok.js@gmail.com>
2023-03-09 22:31:21 +0900

    Clustering and tidying up the code

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ const app = express();
 const port = 3000;
 
 const config = {
-    timeout: 60000,
-    userAgent:
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    maxConcurrency: 4,
+  timeout: 60000,
+  userAgent:
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+  maxConcurrency: 4,
 };
 
-let cluster;
+(async () => {
+  const cluster = await Cluster.launch({
+    puppeteerOptions: {
+      executablePath: '/usr/bin/chromium-browser',
+      args: ['--no-sandbox'],
+    },
+    concurrency: Cluster.CONCURRENCY_BROWSER,
+    maxConcurrency: config.maxConcurrency,
+  });
 
-app.get('/pdf', async (req, res) => {
+  await cluster.task(async ({ page, data }) => {
+    await page.setViewport({ width: 1920, height: 1080 });
+  });
+
+  app.get('/pdf', async (req, res) => {
     const url = req.query.url;
     if (!url) {
-        res.status(400).send('Missing url parameter');
-        return;
+      res.status(400).send('Missing url parameter');
+      return;
     }
 
     const fullUrl = /^https?:\/\//i.test(url) ? url : `http://${url}`;
@@ -25,57 +37,45 @@ app.get('/pdf', async (req, res) => {
 
     let fileDomain = '';
     try {
-        fileDomain = new URL(fullUrl).hostname;
+      fileDomain = new URL(fullUrl).hostname;
     } catch (error) {
-        console.error(`Invalid URL: ${fullUrl}`);
-        res.status(400).send('Invalid URL');
-        return;
+      console.error(`Invalid URL: ${fullUrl}`);
+      res.status(400).send('Invalid URL');
+      return;
     }
 
     const fileName = `${Date.now()}_${fileDomain}.pdf`;
     const timeoutId = setTimeout(() => {
-        console.error('Request timed out');
-        res.status(408).send('Request timed out');
+      console.error('Request timed out');
+      res.status(408).send('Request timed out');
     }, config.timeout);
 
-    const pdfBuffer = await cluster.execute(fullUrl, async (page, data) => {
-        await page.setUserAgent(config.userAgent); // modify
-        await page.goto(data, { waitUntil: 'networkidle0', timeout: config.timeout });
-        return await page.pdf({ format: 'A4' });
-    });
+    const taskFunction = async ({ page, data }) => {
+      await page.setUserAgent(config.userAgent);
+      await page.goto(data, { waitUntil: 'networkidle0', timeout: config.timeout });
+      return await page.pdf({ format: 'A4' });
+    };
+
+    const pdfBuffer = await cluster.execute(fullUrl, taskFunction);
 
     clearTimeout(timeoutId);
     if (pdfBuffer) {
-        res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
-        res.setHeader('Content-Type', 'application/pdf');
-        res.send(pdfBuffer);
+      res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.send(pdfBuffer);
     } else {
-        res.status(500).send('Error generating PDF');
+      res.status(500).send('Error generating PDF');
     }
-});
+  });
 
-(async () => {
-    cluster = await Cluster.launch({
-        puppeteerOptions: {
-            executablePath: '/usr/bin/chromium-browser',
-            args: ['--no-sandbox'],
-        },
-        concurrency: Cluster.CONCURRENCY_BROWSER,
-        maxConcurrency: config.maxConcurrency,
-    });
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
 
-    await cluster.task(async ({ page, data }) => {
-        await page.setViewport({ width: 1920, height: 1080 });
-    });
-
-    app.listen(port, () => {
-        console.log(`App listening at http://localhost:${port}`);
-    });
-})();
-
-process.on('SIGINT', async () => {
+  process.on('SIGINT', async () => {
     console.log('Closing browser cluster...');
     await cluster.close();
     console.log('Browser cluster closed. Exiting...');
     process.exit(0);
-});
+  });
+})();
